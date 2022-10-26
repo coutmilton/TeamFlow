74eb52641c71cb19b25e737c2a7ca97d068e1227
Milton Luiz <milton.luiz36@hotmail.com>
2022-10-26 10:13:06 -0400

    Add topbar

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,25 @@
+import styles from './topbar.module.css';
+
+function Topbar() {
+    return ( 
+        <div className={styles.main}>
+            <div className={styles.wrapper}>
+                <div className={styles.logo}>
+                    <span>team.flow</span>
+                </div>
+                <div className={styles.menu}>
+                    <span>How it Works</span>
+                    <span>Product</span>
+                    <span>Pricing</span>
+                    <span>Resources</span>
+                    <span>Log in</span>
+                </div>
+                <div className={styles.get}>
+                    <button>Get started free</button>
+                </div>
+            </div>
+        </div>
+     );
+}
+
+export default Topbar;
