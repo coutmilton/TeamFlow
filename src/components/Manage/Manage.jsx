6b0f5e7c8ffa537a2ac7bcfaf3c3ac3cd098819f
Milton Luiz <milton.luiz36@hotmail.com>
2022-10-27 09:06:21 -0400

    Manage update 1

--- src/components/Manage/Manage.jsx
+++ src/components/Manage/Manage.jsx
@@ -1,7 +1,7 @@
 import styles from './style.module.css';
 
 function Manage() {
-    return ( 
+    return (
         <div className={styles.main}>
             <div className={styles.wrapper}>
                 <div className={styles.promo}>
@@ -9,12 +9,12 @@ function Manage() {
                     <span className={styles.get}>Get account of $59</span>
                 </div>
                 <div className={styles.manageText}>
-                    <span>Manage the team</span>
-                    <span>everyone wants to be on</span>
+                    <span>Manage the team<br></br>everyone wants to be on</span>
                 </div>
                 <div className={styles.simple}>
-                    <span>Simple platform that lets you master what great managers do best,</span>
-                    <span>as develop trust, collaborate, and drive team performance.</span>
+                    <span>Simple platform that lets you master what great managers do best,<br>
+                    </br>as develop trust, collaborate, and drive team performance.</span>
+
                 </div>
                 <div className={styles.contact}>
                     <input placeholder="name@yourcompany.com" type="text" />
@@ -22,7 +22,7 @@ function Manage() {
                 </div>
             </div>
         </div>
-     );
+    );
 }
 
 export default Manage;
