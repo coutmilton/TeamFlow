8b30fe08251e10d41843686268cbf5b567350d12
Milton Luiz <milton.luiz36@hotmail.com>
2022-10-30 22:02:55 -0400

    Ajustes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
+import Topbar from './components/Topbar/Topbar'
 import Basebar from "./components/Basebar/Basebar"
 import Make from "./components/Make/Make"
 import Manage from "./components/Manage/Manage"
 import Subbar from "./components/Subbar/Subbar"
 import Team from "./components/Team/Team"
-import Topbar from "./components/Topbar/Topbar"
 import Why from "./components/Why/Why"
 import Work from "./components/Work/Work"
 
